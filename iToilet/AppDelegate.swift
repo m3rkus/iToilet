aa37022eb42ff8081819697f53040c360e8b8e95
Roman <roman@letmecode.org>
2020-01-10 17:17:23 +0300

    Initial Commit

---
+++ iToilet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iToilet
+//
+//  Created by Roman Anistratenko on 10.01.2020.
+//  Copyright © 2020 LetMeCode. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
